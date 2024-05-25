adc43e25ec97f88c0068251dc4bcfb4dd00a7402
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-05-25 17:31:25 +0200

    Initial commit

---
+++ backend/src/test/java/org/ericwubbo/minirecipeapp/MiniRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ericwubbo.minirecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniRecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
