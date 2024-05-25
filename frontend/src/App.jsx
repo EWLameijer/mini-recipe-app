f357e04545c7160d58e7681778527594514ee305
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-05-25 18:34:39 +0200

    1 +2 17:28-17:32=4 min 3-7: 17:33-17:38 = 5 min = 9 min totaal 8-13: 17:39-17:43 = 4 min = 13 min totaal 14-16: 17:44-17:46, 17:47-17:52=7 min = 20 min totaal 17: 17:54-17:59 = 5 min = 25 min totaal 18: 18:00+18:02 + 18:28-18:33 = 7 min = 32 min totaal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,35 @@ import axios from "axios";
 
 function App() {
   const [recipes, setRecipes] = useState([]);
+  const [newTitle, setNewTitle] = useState("");
 
   useEffect(() => {
     axios("http://localhost:8080/api/v1/recipes").then((result) =>
       setRecipes(result.data)
     );
   }, []);
+
+  const submit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:8080/api/v1/recipes", { title: newTitle })
+      .then((result) => {
+        setRecipes([...recipes, result.data]);
+        setNewTitle("");
+      });
+  };
+
+  const updateTitle = (event) => {
+    setNewTitle(event.target.value);
+  };
+
   return (
     <>
       <h1>Mini-recipe-app</h1>
+      <form onSubmit={submit}>
+        <input value={newTitle} onChange={updateTitle} />
+        <button>Add recipe</button>
+      </form>
       <ul>
         {recipes.map((recipe) => (
           <li key={recipe.id}>{recipe.title}</li>
