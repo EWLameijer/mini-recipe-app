8a557e506ed7e628dfbcffb7a241c4265bfa5c19
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-05-25 17:59:56 +0200

    1 +2 17:28-17:32=4 min 3-7: 17:33-17:38 = 5 min = 9 min totaal 8-13: 17:39-17:43 = 4 min = 13 min totaal 14-16: 17:44-17:46, 17:47-17:52=7 min = 20 min totaal 17: 17:54-17:59 = 5 min = 25 min totaal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
-  return <h1>Mini-recipe-app</h1>;
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    axios("http://localhost:8080/api/v1/recipes").then((result) =>
+      setRecipes(result.data)
+    );
+  }, []);
+  return (
+    <>
+      <h1>Mini-recipe-app</h1>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>{recipe.title}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
