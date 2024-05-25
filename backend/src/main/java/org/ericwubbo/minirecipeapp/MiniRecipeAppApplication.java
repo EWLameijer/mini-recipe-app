5a056ca82b78a265b7fefee35376135d76576cab
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-05-25 17:32:52 +0200

    1 +2 17:28-17:32=4 min

--- backend/src/main/java/org/ericwubbo/minirecipeapp/MiniRecipeAppApplication.java
+++ backend/src/main/java/org/ericwubbo/minirecipeapp/MiniRecipeAppApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MiniRecipeAppApplication {
+	// phase 1 and 2 finished
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiniRecipeAppApplication.class, args);
